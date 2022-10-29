940ae753dfe4c6922533d2a162a50366789e3c40
Abhishek Kumar <abhishekk580@gmail.com>
2022-10-29 23:00:09 +0530

    strategy pattern

---
+++ 3. Behavioural/Strategy/Main.java
@@ -0,0 +1,9 @@
+package Strategy;
+
+public class Main {
+    public static void main(String[] args) {
+        var imageStorage = new ImageStorage(new JpegCompressor(),
+                new BlackAndWhiteFilter());
+        imageStorage.store("a");
+    }
+}
